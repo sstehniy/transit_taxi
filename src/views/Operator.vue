75159a4a778ecaa27578494e7953b7c4023a6cb1
Dan Kotov <dkotov@ryerson.ca>
2020-06-19 15:12:26 -0400

    Navbar response to rout

--- src/views/Operator.vue
+++ src/views/Operator.vue
@@ -1,3 +1,30 @@
 <template>
-    <div>OPERATOR</div>
+    <div class="operator">
+        <Header class="header" />
+    </div>
 </template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+/* this.$route.name */
+export default {
+    name: "operator",
+    components: {
+        Header
+    }
+};
+</script>
+
+
+<style scoped>
+.operator {
+    position: relative;
+    min-height: 100vh;
+    min-width: 100vw;
+}
+.operator > * {
+    position: absolute;
+    z-index: 1;
+}
+</style>
